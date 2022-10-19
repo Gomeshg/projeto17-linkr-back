f466c4602f9ecc1ace720a01265e097713fdcb87
Hugo Gomes <gomes.hugo00@gmail.com>
2022-10-18 22:17:12 -0300

    build: express, dotenv, cors, bcrypt, joi pg, string-strip-html, uuid; feat: server startup

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const server = express();
+server.use(json());
+server.use(cors());
+dotenv.config();
+
+server.listen(process.env.PORT, () => {
+  console.log("Servidor rodando na porta " + process.env.PORT);
+});
