a38df3386baeeb05aac029df62c95fd0468a933c
Tukkos <jpfcastro@id.uff.br>
2022-10-21 08:05:00 -0300

    feat: authorization on /timeline post route working

--- src/routers/timelineRouter.js
+++ src/routers/timelineRouter.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { getLinks, postLinks } from '../controllers/timelineController.js';
+import authorizationValidation from '../middlewares/authorizationValidation.js';
 import { isItUrl } from '../middlewares/timelineValidation.js';
 
 const timelineRouter = express.Router();
 
 timelineRouter.post(
     '/timeline',
+    authorizationValidation,
     isItUrl,
     postLinks
 );
