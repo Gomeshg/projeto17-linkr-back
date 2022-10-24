7f3bad519592168bbe791ab64fbaae0f7150ebe3
Hugo Gomes <gomes.hugo00@gmail.com>
2022-10-24 20:42:54 -0300

    init week

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -1,37 +1,35 @@
-import * as userRepository from '../repositories/userRepository.js' 
+import * as userRepository from "../repositories/userRepository.js";
 import connection from "../database/postgres.js";
 
 async function postLinks(req, res) {
-    const link = req.body;
-    let text = link.text;
-    const userId = res.localItens.userId;
+  const link = req.body;
+  let text = link.text;
+  const userId = res.localItens.userId;
 
-    if (link.text === "" || link.text === undefined) {
-        link.text = null;
-    };
-    try {
-
-    await connection.query(`
+  if (link.text === "" || link.text === undefined) {
+    link.text = null;
+  }
+  try {
+    await connection.query(
+      `
         INSERT INTO links ("userId", url, text)
         VALUES ($1, $2, $3);
-        `, [userId, link.url, link.text]
+        `,
+      [userId, link.url, link.text]
     );
 
-
-        res.sendStatus(201);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-};
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 async function getLinks(req, res) {
+  console.log("ENTROU NO GET LINKS");
+  const user = res.localItens;
 
-
-    const user = res.localItens;
-
-    try {
-
-        const {rows} = await connection.query(`
+  try {
+    const { rows } = await connection.query(`
     
         SELECT
             COUNT(likes."linkId") AS "likes",
@@ -54,56 +52,61 @@ async function getLinks(req, res) {
                 users."userName",
                 users."pictureUrl"
             ORDER BY "createDate" DESC
-            LIMIT 20;`
-        )
-        const links = await userRepository.linksUser({id:user.userId});
-        
-        const link2 = await userRepository.linksUser({});
-        
-        link2.map((value)=>{ delete value.createDate; return value  })
+            LIMIT 20;`);
 
-        for (let index = 0; index < rows.length; index++) {
-            rows[index]['likeUser'] = []
-            for (let i = 0; i < links.length; i++) {
-                if(rows[index].id === links[i].linkId ){
-                    rows[index]['boolean']=true;
-                }
-            }
-            
-            for (let i = 0; i < link2.length; i++) {
-                if(rows[index].id === link2[i].id ){
-                    rows[index].likeUser.push(link2[i].userName);
-                }
-            }
-        
+    const links = await userRepository.linksUser({ id: user.userId });
+
+    const link2 = await userRepository.linksUser({});
+
+    link2.map((value) => {
+      delete value.createDate;
+      return value;
+    });
+
+    for (let index = 0; index < rows.length; index++) {
+      rows[index]["likeUser"] = [];
+      for (let i = 0; i < links.length; i++) {
+        if (rows[index].id === links[i].linkId) {
+          rows[index]["boolean"] = true;
         }
-        console.log(rows)
-        res.status(200).send(rows);
-    } catch (error) {
-        
-        res.status(500).send(error.message);
+      }
+
+      for (let i = 0; i < link2.length; i++) {
+        if (rows[index].id === link2[i].id) {
+          rows[index].likeUser.push(link2[i].userName);
+        }
+      }
     }
-};
+
+    res.status(200).send(rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 async function deleteLink(req, res) {
-    const id = req.params.id;
-    const { auth } = req.headers;
-    const token = auth?.replace('Bearer ', '');
-  
-    const rows = await userRepository.getItem({table:"sessions", categori:"token", iten: `'${token}'` })
-  
-    try {
-        await connection.query(`
+  const id = req.params.id;
+  const { auth } = req.headers;
+  const token = auth?.replace("Bearer ", "");
+
+  const rows = await userRepository.getItem({
+    table: "sessions",
+    categori: "token",
+    iten: `'${token}'`,
+  });
+
+  try {
+    await connection.query(
+      `
             DELETE FROM links
             WHERE id = $1
-            `, [id]
-        );
-        res.sendStatus(200);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
- }
- 
-
+            `,
+      [id]
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
 
 export { postLinks, getLinks, deleteLink };
--- src/routers/timelineRouter.js
+++ src/routers/timelineRouter.js
@@ -1,27 +1,19 @@
-import express from 'express';
+import express from "express";
 
-import { deleteLink, getLinks, postLinks } from '../controllers/timelineController.js';
-import authorizationValidation from '../middlewares/authorizationValidation.js';
-import { isItUrl } from '../middlewares/timelineValidation.js';
+import {
+  deleteLink,
+  getLinks,
+  postLinks,
+} from "../controllers/timelineController.js";
+import authorizationValidation from "../middlewares/authorizationValidation.js";
+import { isItUrl } from "../middlewares/timelineValidation.js";
 
 const timelineRouter = express.Router();
 
-timelineRouter.post(
-    '/timeline',
-    authorizationValidation,
-    isItUrl,
-    postLinks
-);
+timelineRouter.post("/timeline", authorizationValidation, isItUrl, postLinks);
 
-timelineRouter.get(
-    '/timeline',
-    authorizationValidation,
-    getLinks
-);
+timelineRouter.get("/timeline", authorizationValidation, getLinks);
 
-timelineRouter.delete(
-    '/deleteTimeline/:id',
-    deleteLink
- ); 
+timelineRouter.delete("/deleteTimeline/:id", deleteLink);
 
 export default timelineRouter;
