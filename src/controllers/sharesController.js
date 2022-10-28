4c915c1753a76660926f79502c1434a49d601372
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-10-28 19:08:48 -0300

    feat: end

---
+++ src/controllers/sharesController.js
@@ -0,0 +1,28 @@
+import * as userRepository from '../repositories/userRepository.js' 
+
+export default async function shares(req,res){
+    try {
+
+        const {userId ,linkId, repostUserId} = res.localItens
+
+        console.log(res.localItens)
+
+        if(linkId!==0 && repostUserId!==0){ 
+            await userRepository.insert({localItens: `shares("linkId","userId","RepostId")`, iten:[linkId, userId , repostUserId]})
+            const link = await userRepository.getItem({table:"links", categori:"id",iten:linkId})
+            await userRepository.insert({localItens: `links("userId", url ,repost, text )`, iten:[userId, link[0].url, true, link[0].text]})
+        
+        }
+
+        const cont = await userRepository.getItem({table:"shares", categori:`"linkId"`,iten:linkId })
+
+        const i = cont.length
+
+        res.send({cont:i}).status(200)
+    
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(400)
+        
+    }
+} 
